963f525c312c8b35eb8c8cd1ad19272b58d7da76
Nikita Kornuk <kornyk62@gmail.com>
2024-04-08 01:38:07 +0200

    add mvvm

---
+++ Translator/Module/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  Translator
+//
+//  Created by NikitaKorniuk   on 08.04.24.
+//
+
+import Foundation
---
+++ Translator/Module/ViewModel/MainViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewModel.swift
+//  Translator
+//
+//  Created by NikitaKorniuk   on 08.04.24.
+//
+
+import Foundation
